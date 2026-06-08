489172421c6ecc40d448d845df8116d05c6302a4
Ludovic CH <ludo.chapelet@hotmail.fr>
2026-06-08 21:09:28 +0200

    Change shape from A1.Flex to A2.Flex in main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -37,7 +37,7 @@ data "oci_core_images" "ubuntu_24_04_arm64" {
   compartment_id           = var.compartment_id
   operating_system         = "Canonical Ubuntu"
   operating_system_version = "24.04"
-  shape                    = "VM.Standard.A1.Flex"
+  shape                    = "VM.Standard.A2.Flex"
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
   state                    = "AVAILABLE"
